a0cacb71e81a60b30e1b1356d5598dd65c05a841
i-am-shank <shashanksinha4444@gmail.com>
2024-08-11 23:42:10 +0530

    Week 2 HTML  :  Form, media-tags, table, web-vitals, navigation

---
+++ 03_Unit_3/02_HTML/12_createAForm/script.js
@@ -0,0 +1,10 @@
+const submitBtn = document.getElementById("submit-btn");
+
+const feedbackType = document.getElementById("feedback-type");
+const message = document.getElementById("message");
+const rating = document.getElementById("rating-label");
+
+submitBtn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log();
+});
